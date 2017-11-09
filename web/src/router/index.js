f6fb0ee88a0d498b014de19f1c2a1dee7d805857
machinly <machinly@outlook.com>
2017-11-09 15:02:42 +0800

    update router

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Main from '@/components/Main.vue'
-
+// 产品数据
 import ProductData from '@/views/ProductData.vue'
+// 用户转化
+import UserReferral from '@/views/UserReferral.vue'
 // Acquisition
 import Acquisition from '@/views/Acquisition.vue'
 import AcqSummary from '@/views/acquisition_views/Summary.vue'
@@ -100,12 +102,17 @@ export default new Router({
       // 用户转化
       path: '/user_referral',
       name: 'user_referral',
-      component: Main,
+      component: UserReferral,
       children: [
         {
-          // 获取总结
+          // 下载
           path: '/useref/download',
           name: 'useref_download',
+          component: Main
+        }, {
+          // 留存
+          path: '/useref/retention',
+          name: 'useref_retention',
           component: Main
         }
       ]
---
+++ web/src/views/UserReferral.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-card class="box-card">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="图片发布" name="useref_download"></el-tab-pane>
+      <el-tab-pane label="视频发布" name="useref_retention"></el-tab-pane>
+    </el-tabs>
+    <router-view></router-view>
+  </el-card>
+</template>
+
+<script>
+  export default {
+    name: 'UserReferral',
+    data () {
+      return {
+        activeName: 'useref_download'
+      }
+    },
+    created () {
+      this.parseRoute()
+    },
+    methods: {
+      handleClick (tab, event) {
+        this.$router.replace({'name': tab.name})
+      },
+      parseRoute () {
+        let name = this.$route.name
+        if (name === 'user_referral') {
+          this.activeName = 'useref_download'
+          this.$router.replace({'name': 'useref_download'})
+        } else {
+          this.activeName = name
+        }
+        console.log(this.$route)
+      }
+    },
+    watch: {
+      '$route': 'parseRoute'
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
