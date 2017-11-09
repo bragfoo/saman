e7a75fa7bf043b9fb67aef86fd695fc2ab58f5bf
teamNoble <kevin1993@qq.com>
2017-11-09 17:11:25 +0800

    add product data views

--- web/src/views/product_data_views/VideoPosted.vue
+++ web/src/views/product_data_views/VideoPosted.vue
@@ -1,23 +1,80 @@
 <template>
-  <el-card class="box-card">
-    <plat-fans-chart></plat-fans-chart>
-  </el-card>
+  <el-row>
+    <ve-line :data="chartData" :settings="chartSettings"></ve-line>
+  </el-row>
 </template>
 
 <script>
-  import PlatFansChart from '../data_views/PlatFansChart.vue'
-
   export default {
-    name: 'Main',
-    components: {
-      PlatFansChart
-    },
+    name: 'UGCVideo',
     data () {
-      return {}
+      return {
+        loading: false,
+        url: 'appUGC',
+        chartData: {
+          columns: ['CreateTime', 'VideoSum'],
+          rows: [
+            {
+              CreateTime: (new Date()).getTime(),
+              Like: 0,
+              CommentSum: 0,
+              ShareSum: 0,
+              PicSum: 0,
+              VideoSum: 0
+            }
+          ]
+        },
+        chartSettings: {
+          labelMap: {
+            CreateTime: '事件',
+            VideoSum: '视频'
+          }
+        }
+      }
     },
     created () {
       this.fetchList()
     },
-    methods: {}
+    methods: {
+      // net io
+      fetchList () {
+        this.$http.get(this.url, {
+          params: {
+            platIds: this.playType
+          }
+        }).then((response) => {
+          this.chartData.rows = []
+          response.data.forEach((row) => {
+            let time = new Date(row.CreateTime)
+            row.CreateTime = (time.getMonth() + 1) + '月' + time.getDate() + '日'
+            this.chartData.rows.push(row)
+          })
+        }).then()
+      },
+      reload () {
+        this.fetchList()
+      }
+    }
   }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
