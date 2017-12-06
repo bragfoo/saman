faa5f239e2f2ca2006c688a1466dd073e625a35c
teamNoble <kevin1993@qq.com>
2017-12-06 11:35:05 +0800

    fix zhencai's bug

--- util/excel/newVideo.go
+++ util/excel/newVideo.go
@@ -26,11 +26,11 @@ func getNewVideoFromData(f *excelize.File) {
 	l = append(l, readPlatform(f, 4, "5a169130ef2d1345db33cdc6", sheet)...)  // 空间音乐
 	l = append(l, readPlatform(f, 35, "5a1690eeef2d1345d21327e9", sheet)...) // 空间体育
 	l = append(l, readPlatform(f, 70, "5a16933cef2d1346121beb0c", sheet)...) // 秒拍
-	l = append(l, readPlatform(f, 91, "5a169398ef2d13461ea201a4", sheet)...) // 微博
+	l = append(l, readPlatformWeibo(f, 91, "5a169398ef2d13461ea201a4", sheet)...) // 微博
 
 	platSum = append(platSum, readPlatformSum(f, sheet, "B2", "5a169130ef2d1345db33cdc6")) // 空间音乐
-	platSum = append(platSum, readPlatformSum(f, sheet, "B2", "5a1690eeef2d1345d21327e9")) // 空间体育
-	platSum = append(platSum, readPlatformSum(f, sheet, "B2", "5a16933cef2d1346121beb0c")) // 秒拍
+	platSum = append(platSum, readPlatformSum(f, sheet, "B33", "5a1690eeef2d1345d21327e9")) // 空间体育
+	platSum = append(platSum, readPlatformSum(f, sheet, "B68", "5a16933cef2d1346121beb0c")) // 秒拍
 
 	fans = append(fans, getNewVideoFans(f, sheet, 22, "5a169130ef2d1345db33cdc6")...) // 空间音乐
 	fans = append(fans, getNewVideoFans(f, sheet, 57, "5a1690eeef2d1345d21327e9")...) // 空间体育
