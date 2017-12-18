3ff71c305f9f4d680536d0cd89fad76644d50438
xinma <xinma@mobvoi.com>
2017-12-18 11:40:37 +0800

    add weekly play data

---
+++ web/src/views/data_views/PlatVideoWeeklyPlayBarChart.vue
@@ -0,0 +1,83 @@
+<template>
+  <el-row style="padding:10px">
+   <ve-histogram :data="chartData" :settings="chartSettings" :title="title"></ve-histogram>
+  </el-row>
+</template>
+
+<script>
+  import 'echarts/lib/component/title'
+
+  export default {
+    props: {
+      start: {
+        type: Number,
+        default: -1
+      },
+      end: {
+        type: Number,
+        default: 0
+      }
+    },
+    name: 'PlatVideoPlayWeeklyBarChart',
+    data () {
+      return {
+        loading: false,
+        url: 'weeklyPlay',
+        chartData: {
+          columns: ['PlatName', 'Sum'],
+          rows: []
+        },
+        chartSettings: {
+          labelMap: {
+            PlatName: '平台名',
+            Sum: '增长数'
+          },
+          label: {
+            normal: {
+              show: true
+            }
+          }
+        },
+        title: {
+          text: '平台视频播放每周增量'
+        }
+      }
+    },
+    created () {
+      this.fetchList()
+    },
+    methods: {
+      // net io
+      fetchList () {
+        let params = {}
+        if (this.start >= 0) {
+          params.start = this.start
+          params.end = this.end
+        }
+        this.$http.get(this.url, {
+          params: params
+        }).then((response) => {
+          this.chartData.rows = []
+          let data = response.data === null ? response.data = [] : response.data
+          if (data !== []) {
+            data.sort((r1, r2) => (r1.PlatIds === r2.PlatIds ? 0 : r1.PlatIds > r2.PlatIds ? 1 : -1))
+          }
+        }).then()
+      },
+      reload () {
+        this.fetchList()
+      },
+      watch_reload (val, oldval) {
+        if (val !== oldval) {
+          this.reload()
+        }
+      }
+    },
+    watch: {
+      start: 'watch_reload'
+      // end: 'watch_reload',
+      // platType: 'watch_reload',
+      // videoType: 'watch_reload'
+    }
+  }
+</script>
